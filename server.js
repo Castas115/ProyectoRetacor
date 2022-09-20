cefeb21682780eb53f52bfbb9dc900459004ffa4
jon <joncastas@gmail.com>
2022-09-20 10:45:11 +0200

    template endpoints flota added

--- server.js
+++ server.js
@@ -23,23 +23,63 @@ app.listen(3000, function () {
 
 app.get('/flotas', (req, res) => {
 
-    connection.query("select * from flota", function(error, results){
-        if (error) throw error;
-        console.log("flotas called.");
+    connection.query("select * from flota", function(err, results){
+        if (err) throw err;
+        console.log("get flotas called.");
 
         res.send((results));
-    })
-})
+    });
+});
 
-
-/*app.get('/flota/:id', (req, res) => {
-    connection.connect();
-
-    connection.query("select * from flota where id, function(error, results){
-        if (error) throw error;
-        console.log("flotas called.");
+// FIXME: Cannot GET /flota
+app.get('/flota/(:id)', (req, res) => {
+     let id = req.params.id;
+    connection.query("select * from flota where id = " + id, function(err, results){
+        if (err) throw err;
+        console.log("get flota called.");
 
         res.send((results));
-    })
-    connection.end();
-})*/
+    });
+});
+
+
+app.post('/add', function(req, res, next) {
+    let flota_data = {
+        nombre: req.params.nombre,
+        
+    }
+
+    dbConn.query('INSERT INTO flota SET ?', flota_data, function(err, result) {
+        if(err) throw err
+        console.log("set flota called.");
+
+        res.send((results));
+    });
+});
+
+app.post('/update/(:id)', function(req, res, next) {
+    let flota_data = {
+        id: req.params.id,
+        nombre: req.params.nombre,
+        
+    }
+
+    dbConn.query('UPDATE flota SET ? WHERE id = ' + id, flota_data, function(err, result) {
+        if(err) throw err
+        console.log("set flota called.");
+
+        res.send((results));
+    });
+});
+
+app.get('/delete/(:id)', function(req, res, next) {
+    let id = req.params.id;
+
+    dbConn.query('DELETE FROM flota WHERE id = ' + id, function(err, result) {
+        if(err) throw err
+
+        console.log("delete flota called.");
+
+        res.send((results));
+    });
+});
