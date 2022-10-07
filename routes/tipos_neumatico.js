ba82587aaa4d95b573553c0e185808e81f6b58e7
jon <joncastas@gmail.com>
2022-10-07 10:41:04 +0200

    tipo_neumaticos put endpoint added

--- routes/tipos_neumatico.js
+++ routes/tipos_neumatico.js
@@ -73,6 +73,36 @@ router.post('/', function(req, res, next) {
     }
 })
 
+router.put('/(:id)', function(req, res, next) {
+    let json
+    let id = req.params.id
+    let data = req.body
+
+    if(id.length === 0) {
+        json = {
+            data: undefined,
+            error: "Introduzca el id"
+        }
+        res.statusCode = 400 
+        res.send(json)
+    }else if(Object.keys(data).length == 0) {
+        json = {
+            data: undefined,
+            error: "Introduzca los campos a modificar"
+        }
+        res.statusCode = 400 
+        res.send(json)
+    }else{
+        db.query('UPDATE tipo_neumatico SET ? WHERE id = ' + id, data, function(err, result) {
+            if (err) throw err
+            json ={
+                data: result
+            }
+            res.statusCode = 200
+            res.send(json)
+        })
+    }
+})
 
 router.delete('/(:id)', function(req, res, next) {
     let json
