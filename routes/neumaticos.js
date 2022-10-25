48b124d3024f308cdb76d252ddf7ae324c4511e5
jon <joncastas@gmail.com>
2022-10-25 13:54:46 +0200

    detalle neumatico dev

--- routes/neumaticos.js
+++ routes/neumaticos.js
@@ -14,7 +14,6 @@ router.get('/', (req, res) => {
     })
 })
 
-// TODO: queda por terminar
 router.get('/(:id)', (req, res) => {
     let json
     let id = req.params.id
@@ -26,13 +25,25 @@ router.get('/(:id)', (req, res) => {
         res.statusCode = 400 
         res.send(json)
     }else{ 
-        db.query("SELECT id, id vehiculo, posicion, mm, presion FROM neumatico WHERE id = " + id , function(err, result){
+        db.query(`SELECT tn.dimension, tn.marca, tn.modelo, n.posicion, n.mm, n.presion, v.matricula, 
+        (SELECT b.id_flota FROM base AS b WHERE b.id = v.id_base) AS id_flota
+        FROM neumatico as n 
+        JOIN vehiculo AS v ON v.id = n.id_vehiculo 
+        JOIN tipo_neumatico AS tn ON n.id_tipo_neumatico = tn.id 
+        WHERE n.id = ` + id , function(err, result){
             if (err) throw err
-            json ={
-                data: result
-            }
-            res.statusCode = 200
-            res.send(json)
+
+            db.query("SELECT (SELECT s.nombre FROM tipo_servicio as s WHERE s.id = servicio_neumatico.id_tipo_servicio) as nombre, km_vehiculo, fecha, id_proveedor_servicio, comentario FROM servicio_neumatico WHERE id_neumatico = " + id, (err2, result2) =>{
+
+                result[0].reesculturado = result2.filter(i => i.nombre == "Reesculturado").length != 0
+                json ={
+                    data: result[0]
+                }
+                json.data.servicios= result2
+
+                res.statusCode = 200
+                res.send(json)
+            })
         })
     }
 })
