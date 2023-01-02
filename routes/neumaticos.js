c222e5e9759acb2b7bb3d5b3e1d46559f09f9efb
jon <joncastas@gmail.com>
2023-01-02 10:13:41 +0100

    optimized Neumaticos querying

--- routes/neumaticos.js
+++ routes/neumaticos.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
     let json
     db.query(`SELECT n.id, n.id_vehiculo, n.posicion, n.mm, n.presion, 
     (SELECT IF(count(sn.id) = 0, false, true) FROM servicio_neumatico AS sn WHERE sn.id_neumatico = n.id AND sn.id_tipo_servicio = 7) AS reesculturado
-    FROM neumatico AS n` 
+    FROM neumatico AS n`
     , function(err, result){
         if (err) throw err
         json ={
@@ -28,25 +28,36 @@ router.get('/(:id)', (req, res) => {
         res.statusCode = 400 
         res.send(json)
     }else{ 
-        db.query(`SELECT tn.dimension, tn.marca, tn.modelo, n.posicion, n.mm, n.presion, v.matricula, 
-        (SELECT b.id_flota FROM base AS b WHERE b.id = v.id_base) AS id_flota
+        db.query(`SELECT tn.dimension, tn.marca, tn.modelo, n.posicion, n.mm, n.presion, v.matricula, b.id_flota,
+        s.nombre, sn.km_vehiculo, sn.fecha, sn.id_proveedor_servicio, sn.comentario, 0 AS reesculturado
         FROM neumatico as n 
-        JOIN vehiculo AS v ON v.id = n.id_vehiculo 
-        JOIN tipo_neumatico AS tn ON n.id_tipo_neumatico = tn.id 
+        LEFT JOIN vehiculo AS v ON v.id = n.id_vehiculo 
+        LEFT JOIN base AS b ON b.id = v.id_base
+        LEFT JOIN tipo_neumatico AS tn ON n.id_tipo_neumatico = tn.id 
+        LEFT JOIN servicio_neumatico AS sn ON n.id = sn.id_neumatico
+        LEFT JOIN tipo_servicio as s ON s.id = sn.id_tipo_servicio
         WHERE n.id = ` + id , function(err, result){
             if (err) throw err
 
-            db.query("SELECT (SELECT s.nombre FROM tipo_servicio as s WHERE s.id = servicio_neumatico.id_tipo_servicio) as nombre, km_vehiculo, fecha, id_proveedor_servicio, comentario FROM servicio_neumatico WHERE id_neumatico = " + id, (err2, result2) =>{
+            result = Object.values(result.reduce((r, { dimension, marca, modelo, posicion, mm, presion, matricula, id_flota, reesculturado, ...o }) =>{
+                r[matricula] ??= { dimension, marca, modelo, posicion, mm, presion, matricula, id_flota, reesculturado, servicios: [] }
 
-                result[0].reesculturado = result2.filter(i => i.nombre == "Reesculturado").length != 0
-                json ={
-                    data: result[0]
+                console.log(r)
+                if(o.nombre != null){
+                    r[matricula].servicios.push(o)
+                    if(r[matricula].reesculturado != 1 && o.nombre == "Reesculturado"){
+                        r[matricula].reesculturado = 1
+                    }
                 }
-                json.data.servicios= result2
+                return r
+            }, {}))
 
-                res.statusCode = 200
-                res.send(json)
-            })
+            json ={
+                data: result[0]
+            }
+
+            res.statusCode = 200
+            res.send(json)
         })
     }
 })
